087f737bec888f4e8420b8f55a3098fd7988feac
Juhana Räsänen <juhana.rasanen@precond.com>
2015-11-18 12:58:17 +0200

    __str__ for apiuser and apitoken for clearer admin interface

--- testproject/testproject/test/tests.py
+++ testproject/testproject/test/tests.py
@@ -1,9 +1,10 @@
 # coding=utf-8
 
-from __future__ import unicode_literals
+from __future__ import unicode_literals, print_function
 
 import json
 
+from django.contrib.auth.models import User
 from django.test import TestCase, Client
 from django.utils.datetime_safe import datetime
 from django.utils.timezone import UTC
@@ -14,8 +15,11 @@ from customary.api.models import ApiUser, ApiToken
 class TestApiBase(TestCase):
 
     def setUp(self):
-        self.api_user = ApiUser.objects.create(comment="API test user")
-        self.token = ApiToken.objects.create(api_user=self.api_user, token='1234567890')
+        self.user = User.objects.create_user('user1', 'user@users.com', 'user1', first_name='User1', last_name='User')
+        self.api_user = ApiUser.objects.create(user=self.user, comment='API test user1')
+        self.token = ApiToken.objects.create(api_user=self.api_user, token='1234567890', comment='Status token 1')
+        self.api_user2 = ApiUser.objects.create(comment='API test user2')
+        self.token2 = ApiToken.objects.create(api_user=self.api_user2, token='0987654321', comment='Status token 2')
 
     def api_post(self, c, url, data, expected_code=200, expected_message=None):
         do_request = lambda: c.post(url, data=json.dumps(data), content_type='application/json', follow=True)
@@ -48,26 +52,34 @@ class TestApiBase(TestCase):
 
 class TestApiCore(TestApiBase):
 
+    def test_apiuser_str(self):
+        self.assertEqual(str(self.api_user), 'user1 (API test user1)')
+        self.assertEqual(str(self.api_user2), 'API test user2')
+
+    def test_apitoken_str(self):
+        self.assertEqual(str(self.token), 'user1 (Status token 1)')
+        self.assertEqual(str(self.token2), 'API test user2 (Status token 2)')
+
     def test_status(self):
         c = Client()
         start_time = datetime.now(tz=UTC())
-        self.api_post(c, '/api/status/', data={'token': '1234567890'})
+        self.api_post(c, '/api/status/', data={'token': self.token.token})
         token = ApiToken.objects.get(id=self.token.id)
         self.assertTrue(token.last_seen >= start_time)
 
     def test_status_get(self):
         c = Client()
         start_time = datetime.now(tz=UTC())
-        self.api_get(c, '/api/status/?token=1234567890')
+        self.api_get(c, '/api/status/?token={0}'.format(self.token.token))
         token = ApiToken.objects.get(id=self.token.id)
         self.assertTrue(token.last_seen >= start_time)
 
     def test_status_invalid_token(self):
         self.token.delete()
         c = Client()
-        self.api_post(c, '/api/status/', data={'token': '1234567890'}, expected_code=403, expected_message='Valid token required')
+        self.api_post(c, '/api/status/', data={'token': self.token.token}, expected_code=403, expected_message='Valid token required')
 
     def test_status_invalid_method(self):
         c = Client()
-        response, data = self.api_put(c, '/api/status/', data={'token': '1234567890'}, expected_code=405, expected_message='Method not supported')
+        response, data = self.api_put(c, '/api/status/', data={'token': self.token.token}, expected_code=405, expected_message='Method not supported')
         self.assertEqual(response['Allow'], 'GET, POST')
